c93f93388db2e527202a84ca38ce3435b56b537d
Maciej Skolecki <maciej_skolecki@yahoo.com>
2012-07-10 22:48:20 +0200

    2 implementation of dfs - recursive and iterative. graph reversal implemented as well

--- dfs.rb
+++ dfs.rb
@@ -3,12 +3,64 @@ require 'pp'
 def read_file(filename)
   g = {}
   File.open(filename).each_line do |l|
-    node = l[0].to_i
-    edges = l.split().[1...-1].map {|e| e.to_i}
-    g[node] = edges
+    ln = l.split().map {|e| e.to_i}
+    g[ln[0]] = ln[1..-1]
   end
   return g
 end
 
+# iterative version
+def dfsi(g, start_node)
+  frontier = []
+  visited = []
+  s = start_node
+  frontier.push(s)
+  while !frontier.empty?
+    n = frontier.pop()
+    visited << n
+    p "node: #{n}"
+    for e in g[n]
+      unless (frontier.include?(e) && visited.include?(e))
+        frontier.push(e)
+      end
+    end
+  end
+end
 
-pp read_file('dfs_graph.txt')
+# recursive version, does not mark nodes directly, uses an auxiliary var
+def dfsr(g, s)
+  def _dfsr(g, s, v)
+    n = s
+    puts "node #{n}"
+    for e in g[n]
+      unless v.include?(v)
+        v.push(e)
+        _dfsr(g, e, v)
+      end
+    end
+  end
+  _dfsr(g, s, [])
+end
+
+# reverses graph edges
+def reverse(g) 
+  gt = {}
+  g.each do |k, v|
+    v.each do |e|
+      if !gt[e]
+        gt[e] = [k]
+      else
+        gt[e] += [k]
+      end
+    end
+  end
+  return gt
+end
+
+g = read_file('dfs_graph.txt')
+gt = reverse(g)
+memory_usage = `ps -o rss= -p #{Process.pid}`.to_i / 1000 # in Megabytes
+puts "memory_usage #{memory_usage} MB"
+
+dfsr(g, 1)
+
